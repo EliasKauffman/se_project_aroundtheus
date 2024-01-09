f83de6b9a5cdf9a08ea5ca6869e540383ab3e547
EliasKauffman <eliaskauffman990@gmail.com>
2024-01-09 20:12:33 +0200

    project4 final

--- scripts/index.js
+++ scripts/index.js
@@ -25,6 +25,7 @@ const initialCards = [
   },
 ];
 
+// Elements
 const editButton = document.querySelector(".profile__button_type_edit");
 const modal = document.querySelector(".modal");
 const closeButton = document.querySelector(".modal__close");
@@ -32,15 +33,46 @@ const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 const inputName = document.querySelector("#input-name");
 const inputDescritpion = document.querySelector("#input-description");
+const saveEditForm = modal.querySelector(".modal__form");
+const cardListEL = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+// Functions
+function closeModal() {
+  modal.classList.remove("modal_opened");
+}
 
-console.log(profileDescription.textContent);
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEL = cardElement.querySelector(".card__image");
+  const cardInfoEL = cardElement.querySelector(".card__info");
+  cardImageEL.setAttribute("src", cardData.link);
+  cardImageEL.setAttribute("alt", cardData.name);
+  cardInfoEL.textContent = cardData.name;
+  return cardElement;
+}
 
+// Event Handlers
+function handelProfileEditSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescritpion.value;
+  closeModal();
+}
+
+//Event Listeners
 editButton.addEventListener("click", function () {
   inputName.value = profileName.textContent;
   inputDescritpion.value = profileDescription.textContent;
 
   modal.classList.add("modal_opened");
 });
-closeButton.addEventListener("click", function () {
-  modal.classList.remove("modal_opened");
+
+closeButton.addEventListener("click", closeModal);
+
+saveEditForm.addEventListener("submit", handelProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEL.append(cardElement);
 });
